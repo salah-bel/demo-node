da6c6fc28bba4c6dd675bec78c281a22e5b4bb62
unknown <belasalah@gmail.com>
2023-06-08 16:34:54 +0200

    update project

--- app.js
+++ app.js
@@ -4,63 +4,106 @@ const morgan = require("morgan");
 const logger = require("./logger");
 const path = require("path");
 const mongoose = require("mongoose");
-const ejs = require('ejs')
-const Product = require('./models/Product');
+const ejs = require("ejs");
+const Product = require("./models/Product");
+const Game = require("./models/Game");
 //db
 mongoose
   .connect("mongodb://127.0.0.1:27017/test")
   .then(() => console.log("database Connected!"))
   .catch((err) => console.log("ERROR DB ::", err));
 
-  //test
-
+//test
 
 // middlewares
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'node_modules','bootstrap', 'dist')))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.static(path.join(__dirname, "node_modules", "bootstrap", "dist"))
+);
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-// get all products
+// get all games
 app.get("/", async (req, res) => {
   try {
-    let products = await Product.find();
-    res.render('home', {products});
+    let games = await Game.find();
+    res.render("games", { games });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// get one game and show it
+app.get("/detail-game/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    let game = await Game.findById(req.params.id);
+    console.log(game);
+    res.render("detail-game", { game });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// add new game
+app.get("/new-game", (req, res) => {
+  res.render("new-game");
+});
+
+app.post("/new-product", async (req, res) => {
+  try {
+    let product = new Product(req.body);
+    let newProduct = product.save();
+    console.log(newProduct.name + " s'est bien enregistré");
+    res.redirect("/");
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get('/detail-product/:id', async (req,res) => {
-  console.log(req.params.id);
-  try{
-    let product =await Product.findById(req.params.id);
-    console.log(product)
-    res.render('detail-product' , {product})
-  }
-  catch (error) {
-    console.log(error);
-  }
-})
-
-app.get('/new-product', (req, res)=>{
-  res.render('new-product')
-})
-
-app.post('/new-product', async(req, res)=>{
-  
+// delete one game
+app.get("/delete-game/:id", async (req, res) => {
   try {
-    let product = new Product(req.body)
-    let newProduct = product.save();
-    console.log(newProduct.name + " s'est bien enregistré")
-    res.redirect('/')
-  } catch (error) {
-    console.log(error)
+    let game = await Game.findOneAndDelete({ _id: req.params.id });
+    console.log(game.title, "deleted success");
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
   }
+});
 
+app.get('/edit-game/:id', async (req, res)=>{
+  try {
+    let game = await Game.findById(req.params.id);
+    console.log(game);
+    res.render('new-game', {game})
+  } catch (err) {
+    console.log(err);
+  }
 })
+// edit one game logic
+app.post("/edit-game", async (req, res) => {
+  let id = req.body.id
+  let update = {
+    title: req.body.title,
+    // thumbnail: req.body.thumbnail,
+    // short_description: req.body.short_description,
+    // game_url: req.body.game_url,
+    // genre: req.body.genre,
+    // platform: req.body.platform,
+    // publisher: req.body.publisher,
+    // developer: req.body.developer,
+    // release_date: req.body.release_date,
+    // freetogame_profile_url: req.body.freetogame_profile_url,
+  };
+  try {
+    let game = await Game.findOneAndUpdate({ _id: id }, update);
+    console.log(game.title, "updated success");
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // server
 app.listen(3005, () => {
--- models/Game.js
+++ models/Game.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
-const gameSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-  });
-  const Product = mongoose.model("Product", productSchema);
-  
 
-  module.exports = Product;
+const gameSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  short_description: { type: String, required: true },
+  game_url: { type: String, required: true },
+  genre: { type: String, required: true },
+  platform: { type: String, required: true },
+  publisher: { type: String, required: true },
+  developer: { type: String, required: true },
+  release_date: { type: Date, required: true },
+  freetogame_profile_url: { type: String, required: true }
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
